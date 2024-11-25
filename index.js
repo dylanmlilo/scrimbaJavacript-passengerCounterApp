c59645a8e2d297d0a4ca2a620cc98f662647bca1
dylanmlilo <dylanmlilo12@gmail.com>
2024-11-25 13:46:02 +0200

    changed the name to counter app and added the total and more styling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let total = 0
 
 function increment() {
     count += 1
@@ -10,6 +12,8 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    total += count
+    totalEl.textContent = "Total: " + total
     countEl.textContent = 0
     count = 0
 }
